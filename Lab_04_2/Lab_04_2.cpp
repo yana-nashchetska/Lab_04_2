35feac9b8e84344d01080c06c7724a8d6c05417a
Yana Nashchetska <yana.nashchetska.ri.2022@lpnu.ua>
2022-10-19 23:43:34 +0300

    Lab_04_2 commit

---
+++ Lab_04_2/Lab_04_2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, A, B, y;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(7) << "y" << " |" << endl;
+	cout << "---------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		A = 5 * exp(3 * x);
+		if (x < -1)
+			B = -(sqrt(2) * pow(x, 3) - 7);
+		else
+			if (-1 <= x && x < 3)
+				B = -(2 * log10(1 - x / 4.0));
+			else
+				B = -(cos(abs(x)) + 3);
+		y = A + B;
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
